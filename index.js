fde2d4a3b3aa84c27273b4fae750b46bf92b70c0
Billy <longnd1@bravebits.vn>
2023-10-15 14:39:17 +0700

    update text

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("QuizLove aoi is running");
+  res.send(">>> QuizLove api is running");
 });
 
 // ROUTES
